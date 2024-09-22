74d8011196ff18bd5ca7095d2a125b3687a79ff6
aquinodev2022 <aquino.dev2022@gmail.com>
2024-09-21 21:34:13 -0300

    Codigo base do projeto

---
+++ testeIonic/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    codigo base
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage } from '@ionic/vue';
+</script>
+
+<style scoped>
+/* #container {
+  
+} */
+</style>
